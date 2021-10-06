d1c180d4a8870f5677016e3595f12774776b43da
Ryan Stewart <ryan.stewart@rackspace.com>
2021-10-06 12:01:14 -0500

    fills in basic walk logic
    This makes the walk continue until it reaches a new OID. Still need to
    account for endOfMibView probably.

--- src/snmp_profiler.erl
+++ src/snmp_profiler.erl
@@ -56,26 +56,60 @@ run_switch(Name) ->
 	    {community, snmp_profiler_config:community_string()},
 	    {version,   v2},
 	    {sec_model, v2c}],
-    log(debug, "Register SNMP agent for ~s", [FullName]),
     UserId = "snmp_profiler",
+    log(debug, "Register SNMP agent for ~s", [FullName]),
     case snmpm:register_agent(UserId, FullName, Opts) of
 	ok -> ok;
 	Error ->
 	    Msg = io_lib:format("Failed to register SNMP agent:\n~p", [Error]),
 	    die(Msg, [internal])
     end,
-    Result = instrumented_walk(UserId, FullName, Oid),
+    Result = walk(UserId, FullName, Oid),
     log(debug, "Instrumented walk result: ~p", [Result]),
     snmpm:unregister_agent(UserId, FullName).
 
-instrumented_walk(UserId, TargetName, Oid) ->
-    gen_server:cast(snmp_profiler_stats, {count, "sync_get_next"}),
-    {Time, Result} = timer:tc(fun() -> snmpm:sync_get_next(UserId, TargetName, [Oid]) end),
+walk(UserId, TargetName, Oid) ->
+    walk_r(UserId, TargetName, Oid, Oid, same_oid_retries()).
+
+walk_r(_UserId, _TargetName, _FirstOid, _CurrentOid, 0) ->
+    count("failed_walk"),
+    all_failed;
+walk_r(UserId, TargetName, FirstOid, CurrentOid, Attempts) ->
+    case get_next(UserId, TargetName, CurrentOid) of
+	timeout ->
+	    walk_r(UserId, TargetName, FirstOid, CurrentOid, Attempts - 1);
+	{NextOid, _Value, _IndexThing} ->
+	    case lists:prefix(FirstOid, NextOid) of
+		true -> done;
+		false -> walk_r(UserId, TargetName, FirstOid, NextOid, same_oid_retries())
+	    end
+    end.
+
+get_next(UserId, TargetName, Oid) ->
+    count("sync_get_next"),
+    {Time, Result} =
+	timer:tc(
+	  fun() -> snmpm:sync_get_next(UserId, TargetName, [Oid], single_oid_timeout()) end),
     case Result of
 	{error, {timeout, _}} ->
-	    gen_server:cast(snmp_profiler_stats, {count, "timeout"}),
+	    log(debug, "sync_get_next timeout on ~p", [Oid]),
+	    count("timeout"),
 	    timeout;
-	{ok, {noError, _, [{varbind, _NextOid, _Type, Value, IndexThing}]}, _} ->
-	    gen_server:cast(snmp_profiler_stats, {histo, "response_time", Time/1000}),
-	    {Value, IndexThing}
+	{ok, {noError, _, [{varbind, NextOid, _Type, Value, IndexThing}]}, _} ->
+	    histo("response_time", Time/1000),
+	    {NextOid, Value, IndexThing}
     end.
+
+count(MetricName) ->
+    gen_server:cast(snmp_profiler_stats, {count, MetricName}).
+
+histo(MetricName, Value) ->
+    gen_server:cast(snmp_profiler_stats, {histo, MetricName, Value}).
+
+
+%% @doc Number of times to retry a single oid during a walk. If this number is exceeded,
+%% the walk is considered a failure. Might want to make this configurable.
+same_oid_retries() -> 5.
+
+%% @doc Timeout for a single sync_get_next() call. Might want to make this configurable.
+single_oid_timeout() -> timer:seconds(5).
